8d95dc5fb6e6de48c268c9dbb1c55049aef7a2fd
Atakan Ayyıldız <atakanayyildiz28@gmail.com>
2023-09-09 21:25:52 +0300

    syntax error handled

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   output: "export",
-//   basePath: "/login-app",
+const nextConfig = {
+  // output: "export",
+  // basePath: "/login-app",
   // experimental: {
   // swcPlugins: [["next-superjson-plugin", {}]],
   // },
