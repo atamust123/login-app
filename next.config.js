b8f86c01ab7f794ba78f2ebdd9b4f1f8e5506fe6
Atakan Ayyıldız <atakanayyildiz28@gmail.com>
2023-09-09 19:08:46 +0300

    base path added

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  basePath: "/login-app",
   // experimental: {
   // swcPlugins: [["next-superjson-plugin", {}]],
   // },
